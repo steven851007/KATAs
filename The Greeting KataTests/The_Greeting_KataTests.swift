cc500c85af0386ba4882974955df6e060af74439
Istvan <istvan.balogh@outlook.com>
2022-02-03 11:19:46 +0700

    Add greeting unknown

--- The Greeting KataTests/The_Greeting_KataTests.swift
+++ The Greeting KataTests/The_Greeting_KataTests.swift
@@ -8,8 +8,11 @@
 import XCTest
 import The_Greeting_Kata
 
-func greet(_ name: String) -> String {
-    "Hello, \(name)"
+func greet(_ name: String?) -> String {
+    guard let name = name else {
+        return "Hello, my friend."
+    }
+    return "Hello, \(name)"
 }
 
 class The_Greeting_KataTests: XCTestCase {
@@ -17,6 +20,11 @@ class The_Greeting_KataTests: XCTestCase {
     func test_simpleGreeting() {
         let greeting = greet("Bob")
         XCTAssertEqual(greeting, "Hello, Bob")
+    }
+    
+    func test_greetingUnknown() {
+        let greeting = greet(nil)
+        XCTAssertEqual(greeting, "Hello, my friend.")
     }
 
 }
