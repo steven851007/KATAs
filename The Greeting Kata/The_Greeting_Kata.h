93522b10ff76d76dc90dc7dbd43f1a673e24d9e7
Istvan <istvan.balogh@outlook.com>
2022-02-03 11:14:36 +0700

    Initial Commit

---
+++ The Greeting Kata/The_Greeting_Kata.h
@@ -0,0 +1,18 @@
+//
+//  The_Greeting_Kata.h
+//  The Greeting Kata
+//
+//  Created by Istvan Balogh on 03.02.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for The_Greeting_Kata.
+FOUNDATION_EXPORT double The_Greeting_KataVersionNumber;
+
+//! Project version string for The_Greeting_Kata.
+FOUNDATION_EXPORT const unsigned char The_Greeting_KataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <The_Greeting_Kata/PublicHeader.h>
+
+
