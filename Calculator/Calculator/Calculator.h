ef1363c05ea39c1c7f92bb47b3de15ef0991f319
Istvan <istvan.balogh@outlook.com>
2022-02-03 17:57:26 +0700

    Add Calculator project

---
+++ Calculator/Calculator/Calculator.h
@@ -0,0 +1,18 @@
+//
+//  Calculator.h
+//  Calculator
+//
+//  Created by Istvan Balogh on 03.02.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Calculator.
+FOUNDATION_EXPORT double CalculatorVersionNumber;
+
+//! Project version string for Calculator.
+FOUNDATION_EXPORT const unsigned char CalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calculator/PublicHeader.h>
+
+
