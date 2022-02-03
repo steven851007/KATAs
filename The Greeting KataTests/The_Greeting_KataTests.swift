28c4fd96af340822e79a9054dfd53c9fde91e3fc
Istvan <istvan.balogh@outlook.com>
2022-02-03 12:51:05 +0700

    Rename result to greeting

--- The Greeting KataTests/The_Greeting_KataTests.swift
+++ The Greeting KataTests/The_Greeting_KataTests.swift
@@ -11,54 +11,54 @@ import The_Greeting_Kata
 class The_Greeting_KataTests: XCTestCase {
     
     func test_simpleGreeting() {
-        expect(name: "Bob", result: "Hello, Bob")
+        expect(name: "Bob", greeting: "Hello, Bob")
     }
     
     func test_greetingUnknown() {
-        expect(name: nil, result: "Hello, my friend.")
-        expect(names: [], result: "Hello, my friend.")
+        expect(name: nil, greeting: "Hello, my friend.")
+        expect(names: [], greeting: "Hello, my friend.")
     }
     
     func test_shouting() {
-        expect(name: "JERRY", result: "HELLO JERRY!")
+        expect(name: "JERRY", greeting: "HELLO JERRY!")
     }
     
     func test_oneName() {
-        expect(names: ["Jill"], result: "Hello, Jill")
+        expect(names: ["Jill"], greeting: "Hello, Jill")
     }
     
     func test_twoNames() {
-        expect(names: ["Jill", "Jane"], result: "Hello, Jill and Jane.")
+        expect(names: ["Jill", "Jane"], greeting: "Hello, Jill and Jane.")
     }
     
     func test_multipleNames() {
-        expect(names: ["Amy", "Brian", "Charlotte"], result: "Hello, Amy, Brian, and Charlotte.")
-        expect(names: ["Amy", "Brian", "Charlotte", "Bob"], result: "Hello, Amy, Brian, Charlotte, and Bob.")
+        expect(names: ["Amy", "Brian", "Charlotte"], greeting: "Hello, Amy, Brian, and Charlotte.")
+        expect(names: ["Amy", "Brian", "Charlotte", "Bob"], greeting: "Hello, Amy, Brian, Charlotte, and Bob.")
     }
     
     func test_multipleNamesWithShouting() {
-        expect(names: ["Amy", "BRIAN", "Charlotte"], result: "Hello, Amy and Charlotte. AND HELLO BRIAN!")
-        expect(names: ["Amy", "BRIAN", "Charlotte", "BOB"], result: "Hello, Amy and Charlotte. AND HELLO BRIAN, BOB!")
+        expect(names: ["Amy", "BRIAN", "Charlotte"], greeting: "Hello, Amy and Charlotte. AND HELLO BRIAN!")
+        expect(names: ["Amy", "BRIAN", "Charlotte", "BOB"], greeting: "Hello, Amy and Charlotte. AND HELLO BRIAN, BOB!")
     }
     
     func test_nameWithComma() {
-        expect(names: ["Bob", "Charlie, Dianne"], result: "Hello, Bob, Charlie, and Dianne.")
+        expect(names: ["Bob", "Charlie, Dianne"], greeting: "Hello, Bob, Charlie, and Dianne.")
     }
     
     func test_namesWithQuotes() {
-        expect(names: ["Bob", "\"Charlie, Dianne\""], result: "Hello, Bob, Charlie, and Dianne.")
+        expect(names: ["Bob", "\"Charlie, Dianne\""], greeting: "Hello, Bob, Charlie, and Dianne.")
     }
     
     // MARK: Helpers
     
-    private func expect(name: String?, result: String, file: StaticString = #file, line: UInt = #line) {
+    private func expect(name: String?, greeting: String, file: StaticString = #file, line: UInt = #line) {
         let greeting = Greeter().greet(name)
-        XCTAssertEqual(greeting, result)
+        XCTAssertEqual(greeting, greeting)
     }
     
-    private func expect(names: [String], result: String, file: StaticString = #file, line: UInt = #line) {
+    private func expect(names: [String], greeting: String, file: StaticString = #file, line: UInt = #line) {
         let greeting = Greeter().greet(names)
-        XCTAssertEqual(greeting, result, file: file, line: line)
+        XCTAssertEqual(greeting, greeting, file: file, line: line)
     }
 
 }
