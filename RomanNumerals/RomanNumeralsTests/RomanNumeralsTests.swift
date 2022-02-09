83589f82a54e854371ba379e3965700550e6daff
Istvan <istvan.balogh@outlook.com>
2022-02-09 10:35:36 +0700

    Refactoring

--- RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
+++ RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
@@ -16,32 +16,22 @@ class RomanNumberCalculator {
         
         return string.reduce(0) { partialResult, character in
             prevCharValue = currentCharValue
-            currentCharValue = self.value(of: character)
+            currentCharValue = values[character] ?? 0
             
             let substract = prevCharValue < currentCharValue ? 2*prevCharValue : 0
             return partialResult + currentCharValue - substract
         }
     }
-    private static func value(of character: Character) -> Int {
-        switch character {
-        case "I":
-            return 1
-        case "V":
-            return 5
-        case "X":
-            return 10
-        case "L":
-            return 50
-        case "C":
-            return 100
-        case "D":
-            return 500
-        case "M":
-            return 1000
-        default:
-            return 0
-        }
-    }
+    
+    private static let values: [Character: Int] = [
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    ]
 }
 
 class RomanNumeralsTests: XCTestCase {
